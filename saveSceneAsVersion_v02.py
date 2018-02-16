aa3a832b5bea7942c4319c2b7f917b698f09c4d0
zeth willie <zethwillie@gmail.com>
2018-02-16 12:24:30 -0500

    Initial commit

---
+++ saveSceneAsVersion_v02.py
@@ -0,0 +1,17 @@
+#save file with automatic versioning
+import maya.cmds as mc
+import os
+
+import maya.cmds as mc
+import os
+
+import Utilities.versionFile_v02 as vf
+import Utilities.getFilePath_v01 as gfp
+reload(vf)
+reload(gfp)
+ver=vf.versionClass()
+
+def run(*args, **kwargs):
+    #0=save file for the get file fucntion
+    filePath=gfp.getFilePath(0)
+    ver.versionUp(filePath)
