2eb837a7ba62bab77395b9a5c3128ef695b85996
zeth willie <zethwillie@gmail.com>
2018-02-16 16:52:46 -0500

    deleting all so I can restart with production code

--- Utilities/printMayaScriptPaths_v01.py
+++
@@ -1,9 +0,0 @@
-import maya.cmds as mc
-import os
-
-def run(*args, **kwargs):
-	env="MAYA_SCRIPT_PATH"
-	print "Printing %s Paths..."%env
-	print "-----------"
-	for i in os.environ.get(env).split(";"):
-		print i
