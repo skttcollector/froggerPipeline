b147b1048341f960ee0e4c314a998ed82bc90eca
zeth willie <zethwillie@gmail.com>
2018-03-02 17:07:33 -0500

    some todo stuff

--- todoList.py
+++ todoList.py
@@ -4,8 +4,13 @@ re-adjust naming convention on anim publishes (line up Asset with stage MBs)
 asset manager - import option
 				- variant referencing
 				- replace ref option? this should convert namespaces. . . 
+				- fix intitializing issues on load
+
+file manager - if file has been published, make it italics
 
 check out game exporter stuff re: 2017, 2018
+
+on model export, clean up a bit, pivot to origin, freeze transforms, etc
 
 more generic way to get/set env variables - mostly there. . . do fileManager (where it gets buttons, procedural buttons), if there is something in the variables, then don't need the change project window?
 
@@ -15,8 +20,18 @@ make a tool for exporting indiv clips from anim file
 
 documentation
 
-Asset anim - maybe this hsould be selection based also? 
+Asset anim - maybe this hsould be selection based also? what to do with imported stuff
 
+PUBLISH:
+
+figure out how to make the automate-able
+
+on publish:
+	Notes for shots on publish (or even version up)
+	give option to rename the fbx publish (checkboxes for each token)
+	all should publish to folders?
+	send an email or slack notice
+	
 get enter press working for gameexporter stuff
 
 """
